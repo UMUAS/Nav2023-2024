26b2380f90b78b343676921e313b39aadae988cf
Nakul Sodhi <nakulsodhi@protonmail.com>
2024-02-18 12:46:47 -0600

    initialized package

---
+++ flight_termination/__init__.py
@@ -0,0 +1,3 @@
+# the modules will be imported when import flight_termination.* is called.
+# if __all__ is not defined, flight_termination.* imports nothing.
+__all__ = ['sample_module']
---
+++ flight_termination/sample_module.py
@@ -0,0 +1,7 @@
+# defining __all__ here is optional. if you dont define it, every non underscore object
+# will be imported.
+# Suggestion: Don't define __all__ and just name private classes, methods and vars with __
+
+class sample_module:
+    def test():
+        print("it works!")
