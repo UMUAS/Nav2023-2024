72185903db6cac6e1f7a39e8828b1c5c26dd893b
Oghenetega Okene <okenetega@gmail.com>
2024-02-18 19:13:15 -0600

    Add template for writing tests

---
+++ flight_termination/tests/gcs_client/test_gcs_connection.py
@@ -0,0 +1,9 @@
+import unittest
+
+
+class TestGCSConnection(unittest.TestCase):
+    def test_connect_to_drone(self):
+        self.assertTrue(1 == 1)
+
+    def test_send_command_to_drone(self):
+        self.assertTrue(1 == 1)
