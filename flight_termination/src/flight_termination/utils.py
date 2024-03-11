500e864c5113abc014b75354b3cba7b5bc72a78f
Yonas Jonk <jonkyonas@gmail.com>
2024-03-11 16:49:05 -0500

    Test making changes locally

--- flight_termination/src/flight_termination/utils.py
+++ flight_termination/src/flight_termination/utils.py
@@ -2,11 +2,3 @@ MIN_LATITUDE = -90
 MAX_LATITUDE = 90
 MIN_LONGITUDE = -180
 MAX_LONGITUDE = 180
-
-
-def valid_latitude_and_longitude(lat, lon):
-    if lat < MIN_LATITUDE or lat > MAX_LATITUDE:
-        return False
-    if lon < MIN_LONGITUDE or lon > MAX_LONGITUDE:
-        return False
-    return True
