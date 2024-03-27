b7e009a9d65fc8c677af06c739e7a64f55e63244
Oghenetega Okene <okenetega@gmail.com>
2024-03-26 21:20:20 -0500

    Add dummy methods for handling the termination of the flight

--- flight_termination/src/flight_termination/flight_termination.py
+++ flight_termination/src/flight_termination/flight_termination.py
@@ -80,13 +80,22 @@ class GCSConnection(Connection):
         self.conn = connect_to_gcs(self.conn_string, self.baudrate)
 
 
+def begin_flight_termination():
+    pre_flight_termination()
+    # Set flight to Hold for some time.
+    # Then start flight termination.
+
+
 def pre_flight_termination():
     pass
 
 
-def begin_flight_termination():
-    pre_flight_termination()
-    # Set flight mode to stable.
+def terminate_flight():
+    pass
+
+
+def post_flight_termination():
+    pass
 
 
 def handle_connection_health(conn):
