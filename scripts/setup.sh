844cfa9868010321096fa3f5d2eac72b6de7a7db
Oghenetega Okene <okenetega@gmail.com>
2024-04-30 10:31:39 -0500

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,20 +4,22 @@
 
 set -e
 
-echo "Updating list of available packages..."
-sudo apt update
-sudo apt -y upgrade
+# echo "Updating list of available packages..."
+# sudo apt update
+# sudo apt -y upgrade
 
-echo "Installing pip..."
-sudo apt install -y python3-pip
+# echo "Installing pip..."
+# sudo apt install -y python3-pip
 
-echo "Creating and activating a Python virtual environment..."
-venv_dir=".venv"
-python3 -m venv $venv_dir
-source $venv_dir/bin/activate
+# echo "Creating and activating a Python virtual environment..."
+# venv_dir=".venv"
+# python3 -m venv $venv_dir
+# source $venv_dir/bin/activate
 
 echo "Adding PYTHONPATH environment variable to shell config file..."
-export PYTHONPATH="${PYTHONPATH}:/ABSOLUTE/PATH/TO/Nav2023-2024/flight_termination/src/"
+# Get the absolute path of the parent directory.
+root_dir="$(realpath "$(dirname "$(pwd)")")"
+export PYTHONPATH="${PYTHONPATH}:$root_dir"
 
 echo "Installing dependencies using pip..."
 python -m pip install -r ../requirements.txt
