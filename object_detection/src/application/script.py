ae524b253924a4d2eb51ecfaf503fc0514b5ffb1
ThePyLord <geraldfeu@gmail.com>
2024-05-17 11:07:29 -0500

    update Detection class to avoid using pipes.

--- object_detection/src/application/script.py
+++ object_detection/src/application/script.py
@@ -88,3 +88,73 @@ class ObjectDetection:
 
 
         return direction
+
+
+class Detection:
+    MODEL_PATH = "../../object_detection_model/yolov5s.pt"
+    img_array = []
+    current_direction = ''
+
+    def __init__(self) -> None:
+        self.model = torch.hub.load('ultralytics/yolov5', 'custom', self.MODEL_PATH)
+        
+        self.target_coords = (None, None) # Initialize target coordinates to None
+        print("------ Model loaded successfully ------")
+
+    def get_direction(self, img):
+        direction = "None"
+        if len(self.img_array) == 50:
+            self.img_array.pop(0)
+            self.img_array.append(img)
+            direction = self.process_frame()
+        else:
+            self.img_array.append(img)
+
+        return direction
+
+    def update_target_coords(self, coords):
+        self.target_coords = coords
+
+    def process_frame(self):
+        # print('------- Performing Inference ----------')
+        first_frame = self.model(self.img_array[0])
+        last_frame = self.model(self.img_array[-1])
+
+        first_bbox = first_frame.xyxy[0]
+        last_bbox = last_frame.xyxy[0]
+
+
+        # a1 and a2 are for using the area of the bounding box to determine
+        # proximity to the landing pad
+        # We can use x_width_1 or x_width_2 instead of area
+        a1 = 0
+        a2 = 0
+        x_width_1 = 0
+        x_width_2 = 0
+
+        for box in first_bbox:
+            x1, y1, x2, y2 = box[:4]
+            x_width_1 = (x2 - x1)
+            a1 = (x2 - x1) * (y2 - y1)
+
+        for box in last_bbox:
+            x1, y1, x2, y2 = box[:4]
+            x_width_2 = (x2 - x1)
+            a2 = (x2 - x1) * (y2 - y1)
+
+        direction = None
+        print(f"new: {x_width_1}, old: {x_width_2}", end = ' ')
+        if a1 and a2:
+            if a2 > a1:
+                direction = 'moving closer'
+                self.current_direction = direction
+            elif a2 < a1:
+                direction = 'moving away'
+                self.current_direction = direction
+            else:
+                direction = 'stationary'
+                # self.current_direction = 'stationary'
+        else:
+            direction = 'could not detect box'
+        print(f"Direction: {direction}", end = ' ')
+        return direction
