677f77c90a9929dbaecf8ee5b32ccf5739ecac81
Oghenetega Okene <okenetega@gmail.com>
2024-08-30 22:03:32 -0500

    Changes to make the code PEP8 compliant, not improve it

--- object_detection/src/application/script.py
+++ object_detection/src/application/script.py
@@ -1,8 +1,4 @@
-import time
-import cv2
 import torch
-from PIL import Image
-import numpy as np
 
 
 class ObjectDetection:
@@ -15,7 +11,7 @@ class ObjectDetection:
     def __init__(self, pipe):
         self.pipe = pipe
         # Load the model
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom', self.MODEL_PATH)
+        self.model = torch.hub.load("ultralytics/yolov5", "custom", self.MODEL_PATH)
         self.current_largest_area = 0  # Initialize the largest area
         self.current_largest_distance = 0  # Initialize the largest distance
         self.images = 0
@@ -24,8 +20,8 @@ class ObjectDetection:
     def getDirection(self):
         while True:
             image = self.pipe.recv()
-           
-            if(len(self.image_array) == 20):
+
+            if len(self.image_array) == 20:
                 direction = self.process()
                 self.image_array.pop(0)
                 self.image_array.append(image)
@@ -34,21 +30,24 @@ class ObjectDetection:
                 self.image_array.append(image)
                 self.pipe.send("calculating")
 
-    def detect_center(self,frame,x1,x2,y1,y2):
-        #get midpoint
-        center_x = (int(x2)+int(x1))//2
-        center_y = (int(y2)+int(y1))//2
-        pad_coordinates = (center_x,center_y)
-        #get center of the frame
-        (frame_height, frame_width) = frame.shape[:2] #w:image-width and h:image-height
-        center_coordinates = (frame_width//2, frame_height//2)
-        if(center_coordinates[0] == pad_coordinates[0] and center_coordinates[1] == pad_coordinates[1]):
-            return (True,0,0)
+    def detect_center(self, frame, x1, x2, y1, y2):
+        # get midpoint
+        center_x = (int(x2) + int(x1)) // 2
+        center_y = (int(y2) + int(y1)) // 2
+        pad_coordinates = (center_x, center_y)
+        # get center of the frame
+        (frame_height, frame_width) = frame.shape[:2]  # w:image-width and h:image-height
+        center_coordinates = (frame_width // 2, frame_height // 2)
+        if (
+            center_coordinates[0] == pad_coordinates[0]
+            and center_coordinates[1] == pad_coordinates[1]
+        ):
+            return (True, 0, 0)
         else:
             print(center_coordinates[1], center_y)
-            x_distance = center_x-center_coordinates[0]
+            x_distance = center_x - center_coordinates[0]
             y_distance = center_y - center_coordinates[1]
-            return (False,x_distance,y_distance)
+            return (False, x_distance, y_distance)
 
     def process(self):
         # Convert the received image
@@ -59,28 +58,24 @@ class ObjectDetection:
         results_end = self.model(self.image_array[-1])
         bounding_boxes_start = results_start.xyxy[0]
         bounding_boxes_end = results_end.xyxy[0]
-       
 
         # Calculate the area of each bounding box
-        x_distance_start = 0 
-        x_distance_end = 0 
-
-        
+        x_distance_start = 0
+        x_distance_end = 0
 
         for box in bounding_boxes_start:
             x1, y1, x2, y2, _, _ = box
-            
-            x_distance_start = (x2 - x1)
-       
+
+            x_distance_start = x2 - x1
+
         for box in bounding_boxes_end:
             x1, y1, x2, y2, _, _ = box
-            res =self.detect_center(self.image_array[-1],x1,x2,y1,y2)
-            if(res[0]):
+            res = self.detect_center(self.image_array[-1], x1, x2, y1, y2)
+            if res[0]:
                 print("pad in the centre")
             else:
                 print(f"x distance:{res[1]} y distance {res[2]}")
-            x_distance_end = (x2 - x1)
-                
+            x_distance_end = x2 - x1
 
         # for box in bounding_boxes:
         #     x1, y1, x2, y2, _, _ = box
@@ -109,19 +104,18 @@ class ObjectDetection:
         else:
             direction = "could not detect box"
 
-
         return direction
 
 
 class Detection:
     MODEL_PATH = "../../object_detection_model/yolov5s.pt"
     img_array = []
-    current_direction = ''
+    current_direction = ""
 
     def __init__(self) -> None:
-        self.model = torch.hub.load('ultralytics/yolov5', 'custom', self.MODEL_PATH)
-        
-        self.target_coords = (None, None) # Initialize target coordinates to None
+        self.model = torch.hub.load("ultralytics/yolov5", "custom", self.MODEL_PATH)
+
+        self.target_coords = (None, None)  # Initialize target coordinates to None
         print("------ Model loaded successfully ------")
 
     def get_direction(self, img):
@@ -146,7 +140,6 @@ class Detection:
         first_bbox = first_frame.xyxy[0]
         last_bbox = last_frame.xyxy[0]
 
-
         # a1 and a2 are for using the area of the bounding box to determine
         # proximity to the landing pad
         # We can use x_width_1 or x_width_2 instead of area
@@ -157,27 +150,27 @@ class Detection:
 
         for box in first_bbox:
             x1, y1, x2, y2 = box[:4]
-            x_width_1 = (x2 - x1)
+            x_width_1 = x2 - x1
             a1 = (x2 - x1) * (y2 - y1)
 
         for box in last_bbox:
             x1, y1, x2, y2 = box[:4]
-            x_width_2 = (x2 - x1)
+            x_width_2 = x2 - x1
             a2 = (x2 - x1) * (y2 - y1)
 
         direction = None
-        print(f"new: {x_width_1}, old: {x_width_2}", end = ' ')
+        print(f"new: {x_width_1}, old: {x_width_2}", end=" ")
         if a1 and a2:
             if a2 > a1:
-                direction = 'moving closer'
+                direction = "moving closer"
                 self.current_direction = direction
             elif a2 < a1:
-                direction = 'moving away'
+                direction = "moving away"
                 self.current_direction = direction
             else:
-                direction = 'stationary'
+                direction = "stationary"
                 # self.current_direction = 'stationary'
         else:
-            direction = 'could not detect box'
-        print(f"Direction: {direction}", end = ' ')
+            direction = "could not detect box"
+        print(f"Direction: {direction}", end=" ")
         return direction
